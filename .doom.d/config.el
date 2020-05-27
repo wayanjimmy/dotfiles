d95460862fa6c311a67b0e8792048e92e15bed3c
wayanjimmy <jimmyeatcrab@gmail.com>
2020-05-27 13:17:00 +0700

    (doom-emacs): update default font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :size 14))
+(setq doom-font (font-spec :family "Jetbrains mono" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -78,4 +78,4 @@
 
 (setq geiser-default-implementation 'racket)
 
-(setq exec-path (append exec-path '("/var/folders/f1/3qjvc5895319405rbmrbcrd00000gn/T/fnm-shell-1257907/bin/node")))
+(setq exec-path (append exec-path '("/var/folders/f1/3qjvc5895319405rbmrbcrd00000gn/T/fnm-shell-3279038/bin/node")))
