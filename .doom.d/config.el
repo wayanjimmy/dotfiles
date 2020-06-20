c9ed45b47303736fd2090bbfc79c6dae1689cbf5
wayanjimmy <jimmyeatcrab@gmail.com>
2020-06-20 09:14:19 +0700

    (doomemacs): increase font size

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Jetbrains mono" :size 14))
+(setq doom-font (font-spec :family "Jetbrains mono" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
